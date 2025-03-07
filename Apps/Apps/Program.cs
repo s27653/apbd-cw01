3f59e55f0b1a9afaae273c14548b63e707db2f4d
s27653@pjwstk.edu.pl <s27653@pjwstk.edu.pl>
2025-03-07 16:44:25 +0100

    Change3

--- Apps/Apps/Program.cs
+++ Apps/Apps/Program.cs
@@ -1,3 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, Change!");
 Console.WriteLine("Witam, Zmiano!");
+Console.WriteLine("Witam, Zmiano druga!");
